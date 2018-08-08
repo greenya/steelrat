1485db5840121de722a0f79948c8f914526f8a13
grinevich <yuriy.grinevich@novage.com.ua>
2018-08-08 19:44:00 +0300

    dist folder made root

--- src/components/PageNotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-container grid-list-xl v-once>
-    <v-layout wrap>
-      <v-flex xs12 title>Страница не найдена</v-flex>
-      <v-flex xs12>Не удалось найти запрашиваемую страницу :(</v-flex>
-    </v-layout>
-  </v-container>
-</template>
